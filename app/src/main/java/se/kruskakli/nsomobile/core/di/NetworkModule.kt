8838ae22bd0ce85d5be6f6ce19993c7524503fc5
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-03-10 23:46:59 +0100

    Still getting that runtime logback error

--- app/src/main/java/se/kruskakli/nsomobile/core/di/NetworkModule.kt
+++ app/src/main/java/se/kruskakli/nsomobile/core/di/NetworkModule.kt
@@ -4,21 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonNamingStrategy
 import org.koin.dsl.module
 
 
-//@OptIn(ExperimentalSerializationApi::class)
 val networkModule = module {
     single {
         HttpClient(CIO) {
